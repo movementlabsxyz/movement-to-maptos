0136f07d840870e919c704bb2e031147664a4708
Liam Monninger <l.mak.monninger@gmail.com>
2025-04-14 15:35:27 +0800

    fix: provides an initial structure for the migration
    Most of this is templated over from ffs https://github.com/movementlabsxyz/ffs

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# movement-to-maptos
+<div align="center">
+  <pre>
+MOVEMENT => MAPTOS
+  </pre>
+</div>
+
+# `movement-to-maptos`
+
+Tools, tests, and validation of migration from [`movement`](https://github.com/movementlabsxyz/movement) state to [`maptos`](https://github.com/movementlabsxyz/maptos).
+
+## Getting started
+Coming soon!
+
+## Contributing
+
+| Task | Description |
+|------|-------------|
+| [Upcoming Events](https://github.com/movementlabsxyz/movement-to-maptos/issues?q=is%3Aissue%20state%3Aopen%20label%3Apriority%3Ahigh%2Cpriority%3Amedium%20label%3Aevent) | High-priority `event` issues with planned completion dates. |
+| [Release Candidates](https://github.com/movementlabsxyz/movement-to-maptos/issues?q=is%3Aissue%20state%3Aopen%20label%3Arelease-candidate) | Feature-complete versions linked to events. |
+| [Features & Bugs](https://github.com/movementlabsxyz/movement-to-maptos/issues?q=is%3Aissue%20state%3Aopen%20label%3Afeature%2Cbug%20label%3Apriority%3Aurgent%2Cpriority%3Ahigh) | High-priority `feature` and `bug` issues. |
+
+Please see [CONTRIBUTING.md](CONTRIBUTING.md) file for additional contribution guidelines.
+
+## Organization
+
+There are five subdirectories which progressively build on one another for node logic.
+
+1. [`util`](./util): contains utility logic mainly reused in [`migration`](./migration).
+2. [`migration`](./migration): contains the implementation of the migration.
+3. [`tests`](./tests): contains tests cover the cases for the migration. 
---
+++ flake.nix
@@ -0,0 +1,131 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/a7abebc31a8f60011277437e000eebcc01702b9f";
+    rust-overlay.url = "github:oxalica/rust-overlay/75b2271c5c087d830684cd5462d4410219acc367";
+    flake-utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/72db7ea069f055d5c7856aca091179a070201931"; 
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, foundry, crane, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) foundry.overlay ];
+        };
+
+        toolchain = p: (p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = [ "rustfmt" "clippy" ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain(toolchain);
+
+        frameworks = pkgs.darwin.apple_sdk.frameworks;
+
+        # An LLVM build environment
+        buildDependencies = with pkgs; [
+          perl
+          llvmPackages.bintools
+          openssl
+          openssl.dev
+          libiconv 
+          pkg-config
+          libclang.lib
+          libz
+          clang
+          pkg-config
+          protobuf
+          rustPlatform.bindgenHook
+          lld
+          coreutils
+          gcc
+          rust
+          zlib
+          pandoc
+        ] ++ lib.optionals stdenv.isDarwin [
+          fixDarwinDylibNames
+        ];
+        
+        sysDependencies = with pkgs; [] 
+        ++ lib.optionals stdenv.isDarwin [
+          frameworks.Security
+          frameworks.CoreServices
+          frameworks.SystemConfiguration
+          frameworks.AppKit
+          libelf
+        ] ++ lib.optionals stdenv.isLinux [
+          udev
+          systemd
+          bzip2
+          elfutils
+        ];
+
+        testDependencies = with pkgs; [
+          python311
+          just
+          foundry-bin
+          process-compose
+          jq
+          docker
+          solc
+          grpcurl
+          grpcui
+        ];
+
+        # Specific version of toolchain
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rust;
+          rustc = rust;
+        };
+    
+      in {
+        devShells = rec {
+          default = docker-build;
+          docker-build = pkgs.mkShell {
+            ROCKSDB = pkgs.rocksdb;
+            OPENSSL_DEV = pkgs.openssl.dev;
+         
+            buildInputs = with pkgs; [
+              # rust toolchain
+              (toolchain pkgs)
+            ] ++ sysDependencies ++ buildDependencies ++ testDependencies;
+
+            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/";
+
+            shellHook = ''
+              #!/usr/bin/env ${pkgs.bash}
+              # Export linker flags if on Darwin (macOS)
+              if [[ "$(${pkgs.stdenv.hostPlatform.system})" =~ "darwin" ]]; then
+                export LDFLAGS="-L/opt/homebrew/opt/zlib/lib"
+                export CPPFLAGS="-I/opt/homebrew/opt/zlib/include"
+              fi
+
+              # Add ./target/debug/* to PATH
+              export PATH="$PATH:$(pwd)/target/debug"
+
+              # Add ./target/release/* to PATH
+              export PATH="$PATH:$(pwd)/target/release"
+
+               # Copy over ./githooks/pre-commit to .git/hooks/pre-commit
+              cp $(pwd)/.githooks/pre-commit $(pwd)/.git/hooks/pre-commit
+              chmod +x $(pwd)/.git/hooks/pre-commit
+
+              cat <<'EOF'
+                          ------------
+                f ------ |            |
+                f ------ | 5e771e3e47 |
+                5 ------ |            |
+                          ------------
+              EOF
+
+              echo "FFS, just take a vote!"
+            '';
+          };
+        };
+      }
+    );
+}
