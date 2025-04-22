022e322676be1d73cc611ebb562fd777628dca67
Liam Monninger <l.mak.monninger@gmail.com>
2025-04-22 13:29:13 +0800

    chore: remove state keys enabled.

--- migration/core/mtma-indexer-db-statekeys-enabled/README.md
+++
@@ -1,2 +0,0 @@
-# `mtma-null`
-A "null" migration that does nothing but for the Movement Aptos Executor Struct from the Movement Struct. 
--- migration/core/mtma-indexer-db-statekeys-enabled/src/migrate.rs
+++
@@ -1,7 +0,0 @@
-/// Contains the configuration structs and logic for the migration.
-pub mod config;
-/// Contains the logic for the migration.
-pub mod migrate;
-
-pub use config::*;
-pub use migrate::*;
