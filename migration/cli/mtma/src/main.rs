3d6c014ba4fd2f02b786dee58c7c24e47ebd063f
Liam Monninger <l.mak.monninger@gmail.com>
2025-04-23 16:45:56 +0800

    chore: update tracing usage.

--- migration/cli/mtma/src/main.rs
+++ migration/cli/mtma/src/main.rs
@@ -12,6 +12,17 @@ async fn main() -> Result<(), anyhow::Error> {
 	// Load environment variables from .env file.
 	dotenv().ok();
 
+	// initialize tracing env filter (no logs by default)
+	tracing_subscriber::fmt::init();
+	let env_filter = EnvFilter::builder()
+		.with_default_directive(LevelFilter::OFF.into())
+		.from_env_lossy();
+	tracing::subscriber::set_global_default(
+		tracing_subscriber::fmt::Subscriber::builder()
+			.with_env_filter(env_filter)
+			.finish(),
+	);
+
 	// Run the CLI.
 	let movement_to_movement_aptos = cli::MovementToMovementAptos::parse();
 	movement_to_movement_aptos.execute().await?;
