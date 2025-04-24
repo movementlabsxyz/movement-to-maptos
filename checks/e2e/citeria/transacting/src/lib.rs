ae3f27fb4475e6068bac69dba362b8dce9149745
Liam Monninger <l.mak.monninger@gmail.com>
2025-04-24 16:12:42 +0800

    fix: transacting.

--- checks/e2e/citeria/transacting/src/lib.rs
+++ checks/e2e/citeria/transacting/src/lib.rs
@@ -1,15 +1,13 @@
-use aptos_types::account_address::AccountAddress;
-use aptos_types::transaction::EntryFunction;
-use migration_executor_test_types::criterion::{
-	Criterion, CriterionError, Criterionish, MovementAptosExecutor,
-};
-use move_core_types::value::serialize_values;
-use move_core_types::{identifier::Identifier, language_storage::ModuleId, value::MoveValue};
-
 use migration_e2e_test_types::criterion::{
-	movement_aptos_e2e_client::types::LocalAccount as MovementAptosLocalAccount,
-	movement_e2e_client::types::LocalAccount as MovementLocalAccount, Criterion, CriterionError,
-	Criterionish, MovementAptosE2eClient, MovementE2eClient,
+	movement_aptos_e2e_client::move_types::{
+		identifier::Identifier, language_storage::ModuleId, value::MoveValue,
+	},
+	movement_aptos_e2e_client::types::{
+		account_address::AccountAddress, transaction::EntryFunction,
+		LocalAccount as MovementAptosLocalAccount,
+	},
+	movement_e2e_client::types::LocalAccount as MovementLocalAccount,
+	Criterion, CriterionError, Criterionish, MovementAptosE2eClient, MovementE2eClient,
 };
 
 pub struct MaptosTransferLifecycle {
@@ -73,42 +71,68 @@ impl Criterionish for MaptosTransferLifecycle {
 			CriterionError::Internal(format!("Invalid function identifier: {:?}", e).into())
 		})?;
 
-		let signer = movement_aptos_e2e_client.get_signer();
 		let recipient1 = AccountAddress::random();
 		let recipient2 = AccountAddress::random();
 
 		// 1. Maptos executor transfers 1 MOVE to recipient1
-		let tx1_args = serialize_values(&vec![
-			MoveValue::Address(recipient1),
-			MoveValue::U64(100_000_000), // 1 MOVE
-		]);
+		let tx1_args = vec![
+			bcs::to_bytes(&MoveValue::Address(recipient1))
+				.map_err(|e| {
+					CriterionError::Internal(format!("Failed to serialize address: {:?}", e).into())
+				})?
+				.to_vec(),
+			bcs::to_bytes(&MoveValue::U64(100_000_000))
+				.map_err(|e| {
+					CriterionError::Internal(format!("Failed to serialize u64: {:?}", e).into())
+				})?
+				.to_vec(),
+		];
 		let tx1 = EntryFunction::new(module_id.clone(), fn_name.clone(), vec![], tx1_args);
 
 		movement_aptos_e2e_client
-			.simulate_entry_function(&signer, &tx1, vec![])
+			.simulate_entry_function(self.aptos_local_account_mut(), &tx1, vec![])
 			.map_err(|e| {
 				CriterionError::Unsatisfied(format!("Initial funding failed: {:?}", e).into())
 			})?;
 
-		// 2. recipient1 transfers 0 MOVE to themselves
-		let tx2_args = serialize_values(&vec![MoveValue::Address(recipient1), MoveValue::U64(0)]);
+		// 2. transfers 0 MOVE to recipient1
+		let tx2_args = vec![
+			bcs::to_bytes(&MoveValue::Address(recipient1))
+				.map_err(|e| {
+					CriterionError::Internal(format!("Failed to serialize address: {:?}", e).into())
+				})?
+				.to_vec(),
+			bcs::to_bytes(&MoveValue::U64(0))
+				.map_err(|e| {
+					CriterionError::Internal(format!("Failed to serialize u64: {:?}", e).into())
+				})?
+				.to_vec(),
+		];
 		let tx2 = EntryFunction::new(module_id.clone(), fn_name.clone(), vec![], tx2_args);
 
-		maptos_executor
-			.simulate_entry_function(&recipient1, &tx2, vec![])
+		movement_aptos_e2e_client
+			.simulate_entry_function(self.aptos_local_account_mut(), &tx2, vec![])
 			.map_err(|e| {
 				CriterionError::Unsatisfied(format!("Self transfer failed: {:?}", e).into())
 			})?;
 
-		// 3. recipient1 transfers 0.1 MOVE to recipient2
-		let tx3_args = serialize_values(&vec![
-			MoveValue::Address(recipient2),
-			MoveValue::U64(10_000_000), // 0.1 MOVE
-		]);
+		// 3. transfers 0.1 MOVE to recipient2
+		let tx3_args = vec![
+			bcs::to_bytes(&MoveValue::Address(recipient2))
+				.map_err(|e| {
+					CriterionError::Internal(format!("Failed to serialize address: {:?}", e).into())
+				})?
+				.to_vec(),
+			bcs::to_bytes(&MoveValue::U64(10_000_000))
+				.map_err(|e| {
+					CriterionError::Internal(format!("Failed to serialize u64: {:?}", e).into())
+				})?
+				.to_vec(),
+		];
 		let tx3 = EntryFunction::new(module_id, fn_name, vec![], tx3_args);
 
-		maptos_executor
-			.simulate_entry_function(&recipient1, &tx3, vec![])
+		movement_aptos_e2e_client
+			.simulate_entry_function(self.aptos_local_account_mut(), &tx3, vec![])
 			.map_err(|e| {
 				CriterionError::Unsatisfied(
 					format!("Transfer to recipient2 failed: {:?}", e).into(),
