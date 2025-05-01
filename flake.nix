578d02e2c1a1f7bda5a34ccfbabbc8f66885e28d
Liam Monninger <l.mak.monninger@gmail.com>
2025-05-01 11:29:10 +0800

    feat: embed movement in nix flake.

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     foundry.url = "github:shazow/foundry.nix/72db7ea069f055d5c7856aca091179a070201931"; 
     crane.url = "github:ipetkov/crane";
+    movement.url = "github:movementlabsxyz/movement/aa1ffed1a113441a65662792d15682ad52406108";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, foundry, crane, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, foundry, crane, movement, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -21,6 +22,35 @@
         craneLib = (crane.mkLib pkgs).overrideToolchain(toolchain);
 
         frameworks = pkgs.darwin.apple_sdk.frameworks;
+
+        # a function to handle the movement repository
+        # NOTE: this is not a derivation.
+        # We do not use derivations because we need to perform mutable operations on the directory; that's the simples path to embedding movement. 
+        movement = ''
+          # Create .vendors directory if it doesn't exist
+          mkdir -p .vendors
+
+          # Handle Movement repository
+          if [ -d ".vendors/movement" ]; then
+            # Save target directory if it exists
+            if [ -d ".vendors/movement/target" ]; then
+              mv .vendors/movement/target /tmp/movement-target
+            fi
+            # Remove existing directory
+            rm -rf .vendors/movement
+          fi
+
+          # Clone fresh copy
+          git clone https://github.com/movementlabsxyz/movement.git .vendors/movement
+          cd .vendors/movement
+          git checkout ${movement.rev}
+
+          # Restore target directory if it existed
+          if [ -d "/tmp/movement-target" ]; then
+            mv /tmp/movement-target target
+          fi
+          cd ../..
+        '';
 
         # An LLVM build environment
         buildDependencies = with pkgs; [
@@ -113,6 +143,8 @@
 
               # Add ./target/release/* to PATH
               export PATH="$PATH:$(pwd)/target/release"
+
+              ${movement}
 
                # Copy over ./githooks/pre-commit to .git/hooks/pre-commit
               cp $(pwd)/.githooks/pre-commit $(pwd)/.git/hooks/pre-commit
