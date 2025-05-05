d751f29b65c07e103d38b91302431900b0bb240f
Liam Monninger <l.mak.monninger@gmail.com>
2025-05-05 19:29:47 +0800

    feat: use build hooks to prebuild.

--- util/movement/movement-core/build.rs
+++ util/movement/movement-core/build.rs
@@ -1,7 +1,29 @@
-use include_vendor::{Buildtime, BuildtimeError};
+use cargo_metadata::MetadataCommand;
+use include_vendor::{Buildtime, BuildtimeError, HookError, Noop, PreBuildHook};
+use std::process::Command;
+#[derive(Debug, Clone)]
+pub struct BuildMovement;
+
+impl PreBuildHook for BuildMovement {
+	fn before(&self) -> Result<(), HookError> {
+		// in the workspace root .movement/movement run cargo build --all-targets
+		let metadata = MetadataCommand::new().exec().map_err(|e| HookError::Internal(e.into()))?;
+		let workspace_root = metadata.workspace_root;
+		let movement_dir = workspace_root.join(".vendor/movement");
+
+		Command::new("cargo")
+			.current_dir(movement_dir)
+			.args(&["build", "--all-targets"])
+			.status()
+			.map_err(|e| HookError::Internal(e.into()))?;
+
+		Ok(())
+	}
+}
 
 fn main() -> Result<(), BuildtimeError> {
-	let builder = Buildtime::try_new("movement".to_string())?;
+	let mut builder: Buildtime<BuildMovement, Noop> = Buildtime::try_new("movement".to_string())?;
+	builder.include("target");
 
 	builder.build()?;
 
